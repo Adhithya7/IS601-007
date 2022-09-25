4d2254b48acbc3edbeee1e41f7e662575f234331
Adhithya7 <athironaldo@gmail.com>
2022-09-24 22:01:59 -0400

    Added the necessary code for problems

--- M2/problem1.py
+++ M2/problem1.py
@@ -1,3 +1,6 @@
+#UCID: 31584791
+#Date: 09/24/2022
+
 a1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 a2 = [0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
 a3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
@@ -8,7 +11,12 @@ def process_array(num, arr):
     print("\nProcessing Array({}): \n\n".format(num))
     print(arr)
     print("\nOdds output:\n")
-    # TODO add necessary print statement to output only the odd values (hint, best if shown as a single line)
+    #Function finds the odd numbers in the input array and prints the same (by using modulo)
+    temp_arr = []
+    for element in arr:
+        if element % 2 != 0:
+            temp_arr.append(element)
+    print(temp_arr) 
 
 
 print("Problem 1")
--- M2/problem2.py
+++ M2/problem2.py
@@ -1,3 +1,6 @@
+#UCID: 31584791
+#Date: 09/24/2022
+
 a1 = [10.001, 11.591, 0.011, 5.991, 16.121, 0.131, 100.981, 1.001]
 a2 = [1.99, 1.99, 0.99, 1.99, 0.99, 1.99, 0.99, 0.99]
 a3 = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]
@@ -8,9 +11,13 @@ def process_array(num, arr):
     print("\nProcessing Array({}): \n\n".format(num))
     print(arr)
     total = 0.00
-    # TODO add necessary code here for sum; every number must have two decimal places shown
-    print("\nThe total is {}:\n".format(total))
-
+    #Function calucates the sum of an array (upto 2 decimal point precision)
+    #The function also takes into consideration the floating point precision issue in python
+    for num in arr:
+        total += num*1000 
+        #Multiplied by 1000 since the numbers in the array have max 3 floating points
+    total = '%.2f'%round(total/1000, 2) 
+    print("\nThe total is : {}\n".format(total))
 
 print("Problem 2")
 process_array(1, a1)
