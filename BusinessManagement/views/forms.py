3fb67f48cfa4365a27b3b83b0c352be48d5e4909
Adhithya7 <athironaldo@gmail.com>
2022-11-28 16:15:46 -0500

    Removed redundant fileds from forms

--- BusinessManagement/views/forms.py
+++ BusinessManagement/views/forms.py
@@ -5,13 +5,10 @@ from wtforms.validators import DataRequired, Email, InputRequired, EqualTo
 class Employee(FlaskForm):
     first_name = StringField("First_name", [InputRequired()])
     last_name = StringField("Last_name", [InputRequired()])
-    company = IntegerField("Company")
     email = EmailField("Email", [InputRequired(), Email()])
 class Company(FlaskForm):
     name = StringField("name", [InputRequired()])
     address = StringField("address", [InputRequired()])
     city = StringField("city", [InputRequired()])
-    country = StringField("country", [InputRequired()])
-    state = StringField("state", [InputRequired()])
     zip_code = StringField("zip_code", [InputRequired()])
     website = StringField("website")
